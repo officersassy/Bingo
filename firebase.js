e83ccac34afe36ef63afe8c4816ae9c8717c4f76
officersassy <tylerheath35@outlook.com>
2026-08-02 20:37:07 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,54 @@
+// ==========================
+// FIREBASE CONNECTION
+// ==========================
 
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { 
+    getDatabase 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+
+
+
+// YOUR FIREBASE CONFIG
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDme5iZZNPN0O128vw0MP9aGjLZXD3oKy8",
+
+    authDomain: "bingo-5174e.firebaseapp.com",
+
+    databaseURL: "https://bingo-5174e-default-rtdb.europe-west1.firebasedatabase.app",
+
+    projectId: "bingo-5174e",
+
+    storageBucket: "bingo-5174e.firebasestorage.app",
+
+    messagingSenderId: "647984877295",
+
+    appId: "1:647984877295:web:a74b477fa7d46dc9cc551a"
+
+};
+
+
+
+
+
+// START FIREBASE
+
+const app = initializeApp(firebaseConfig);
+
+
+
+// DATABASE CONNECTION
+
+const database = getDatabase(app);
+
+
+
+
+// EXPORT FOR OTHER FILES
+
+export { database };
